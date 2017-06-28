cddc750a516c9b2f2d17813c7c35514481f9b552
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-06-28 12:07:39 -0500

    comentarios cambiados

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var estudiantes = [];
 
 function obtenerListaEstudiantes() {
-    // TO DO: Retornar la lista de estudiantes
-
+    // Obtener la lista de estudiantes
+return estudiantes;
 }
 
 function Persona(nombre, porc_tec, porc_HSE) {
@@ -12,22 +12,21 @@ function Persona(nombre, porc_tec, porc_HSE) {
 };
 
 function agregarEstudiante() {
+  // Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
   var nombreEstudiante = prompt ("Nombre de la estudiante: ");
   var porcTecnico = prompt ("Porcentaje técnico: ");
   var porcHabSocEmo = prompt ("Porcentaje Habilidades Socio-emocionales: ");
-
+  porcTecnico=porcTecnico.toLowerCase();
   var estudiante = new Persona(nombreEstudiante, porcTecnico, porcHabSocEmo);
+  // Estudiante es agregado a la lista de estudiantes
   estudiantes.push(estudiante);
 
   return estudiante;
-    // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
-    // Hint: El estudiante debe ser agregado a la lista de estudiantes
-    // TO DO: Retornar el estudiante recientemente creado
+    // Retorna el estudiante recientemente creado
 
 }
-//
+
 function mostrar(estudiante) {
-    // TO DO: Completar el template para que muestre las propiedades correctas del estudiante
     var resultado = "";
     resultado += "<div class='row'>";
     resultado += "<div class='col m12'>";
@@ -42,45 +41,39 @@ function mostrar(estudiante) {
     resultado += "</div>";
     return resultado;
 }
-//
-function mostrarLista() {
-    // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante)
-    // Retornar el template de todos los estudiantes
+
+function mostrarLista(estudiantes) {
     var template = '';
     for (var i = 0; i < estudiantes.length; i++ ) {
+      //mostrar cada estudiante (objetos) al total de objetos
       var estudiante = estudiantes[i];
       template += mostrar(estudiante);
+      //Devolver en funcion estudiante
     }
 
     return template;
 }
-//
-function buscar(nombre) {
+
+function buscar(nombre,estudiantes) {
+  //Estandarizar nombre
   var nombreMin = nombre.toLowerCase();
-  for (var i = 0; i < estudiantes.length; i++ ) {
-    var estudiante = estudiantes[i];
-    if (estudiante.nombre.toLowerCase() == nombreMin) {
-      console.log(estudiante)
-      return estudiante;
-    }
+    for (var i = 0; i < estudiantes.length; i++ ) {
+      var estudiante = estudiantes[i];
+      if (estudiante.nombre.toLowerCase() == nombreMin) {
+        console.log(estudiante)
+        return estudiante;
+      }
   }
-    // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
-    // Retornar el objeto del estudiante buscado
-    // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
-}
-//
-/* function topTecnico(estudiantes) {
-     estudiantes.sort(function (a,b){
-     return a.porc_tec - b.porc_tec;
+
+
+ function topTecnico(estudiantes) {
+   estudiantes.sort(function(a,b){
+     return a.porc_tec - b.porc_tec
    });
-   return res;
  }
-*/
-//     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-//
-// }
-//
-// function topHSE(estudiantes) {
-//     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-//
-// }
+// 
+ function topHSE(estudiantes) {
+   estudiantes.sort(function(a,b){
+     return a.porc_HSE - b.porc_HSE
+   });
+}
